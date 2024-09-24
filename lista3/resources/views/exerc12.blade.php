80a5fad12b39957a40c1418d3ff2e9aa93d079e9
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-09-23 22:11:01 -0300

    23/09

--- lista3/resources/views/exerc12.blade.php
+++ lista3/resources/views/exerc12.blade.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 2</title>
+    <title>Exercicio 12</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
     <h1>Calculadora!!!</h1>
-    <form action="/respostaexerc2" method="POST">
+    <form action="/respostaexerc12" method="POST">
         @CSRF
         <input type="number" name="valor1"/>
         <input type="number" name="valor2"/>
